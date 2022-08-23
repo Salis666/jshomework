b60f29cf197ff7596eaa8f256b01b1d7f93e2334
salis <vampir.66.66@yandex.ru>
2022-08-23 20:45:44 +0500

    test_homework_n2

--- script.js
+++ script.js
@@ -3,3 +3,7 @@
 let tCelsius = prompt('Введите температуру в фаренгейтах');
 let tFahrenheit = (9 / 5) * tCelsius + 32;
 alert(tFahrenheit);
+
+let name = 'Василий';
+let admin = name;
+alert(admin);
