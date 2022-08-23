8ab373c4750e4a0faca500b16e2f82a3264e7a89
Salis <vampir.66.66@yandex.ru>
2022-08-23 22:06:00 +0500

    Revert "Merge branch 'homework1' into main"
    This reverts commit e8e5ca74246203e7970749d9b41e7be1370ab976.

--- script.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-let tCelsius = prompt('Введите температуру в фаренгейтах');
-let tFahrenheit = (9 / 5) * tCelsius + 32;
-alert(tFahrenheit);
-
-let name = 'Василий';
-let admin = name;
-alert(admin);
