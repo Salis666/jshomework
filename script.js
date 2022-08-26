c4016d8dfee1882b4ab3bc03c6b3c355ff028c32
salis <vampir.66.66@yandex.ru>
2022-08-26 21:44:40 +0500

    p3p4

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 'use strict';
 
-{
+/* {
             let tCelsius = prompt('Введите температуру в фаренгейтах');
             let tFahrenheit = (9 / 5) * tCelsius + 32;
             alert(tFahrenheit);
@@ -10,7 +10,46 @@
             let name = 'Василий';
             let admin = name;
             alert(admin);
-}
+} */
 
 // Домашнее задание 2
 
+/* {
+            let a = +prompt('Введите любое число');
+            let b = +prompt('Введите любое число');
+            if (a > 0) {
+                        if (b > 0) {
+                                    alert(a / b);
+                        } else {
+                                    alert(a + b);
+                        }
+            } else {
+                        if (b > 0) {
+                                    alert(a + b);
+                        } else {
+                                    alert(a * b);
+                        }
+            }
+} */
+
+{
+            let a = 7;
+            switch (a) {
+                        case 0: alert(0);
+                        case 1: alert(1);
+                        case 2: alert(2);
+                        case 3: alert(3);
+                        case 4: alert(4);
+                        case 5: alert(5);
+                        case 6: alert(6);
+                        case 7: alert(7);
+                        case 8: alert(8);
+                        case 9: alert(9);
+                        case 10: alert(10);
+                        case 11: alert(11);
+                        case 12: alert(12);
+                        case 13: alert(13);
+                        case 14: alert(14);
+                        case 15: alert(15);
+            }
+}
